2bafc11efbebced30ffd943a876e4773299c5755
Dayeane <dayeane85@gmail.com>
2021-10-14 15:40:46 -0700

    commit tree 7337de81bfa1dae8a88e529716f3fb5f75ce1664

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ function findMatching(drivers, name) {
     return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
 }
 
-function fuzzyMatch(drivers, text) {
-    return drivers.filter(driver => {
-        return (driver.toLowerCase().slice(0, text.length) === text.toLowerCase());
-    })
-}
+// function fuzzyMatch(drivers, text) {
+//     return drivers.filter(driver => {
+//         return (driver.toLowerCase().slice(0, text.length) === text.toLowerCase());
+//     })
+// }
 
 function matchName(drivers, name) {
     return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
 }
 
 
+function fuzzyMatch(drivers, text) {
+    return drivers.filter(driver => {
+        return driver.startsWith(text)
+    })
+}
